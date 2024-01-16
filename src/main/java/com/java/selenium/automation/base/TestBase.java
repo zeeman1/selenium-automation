747e6edfa25158d6f62129f5a3d8dfbe3f2b9df2
zeeman1 <zelalemgetachen@yahoo.com>
2024-01-16 06:42:50 -0800

    Added code to support multiple browsers

--- src/main/java/com/java/selenium/automation/base/TestBase.java
+++ src/main/java/com/java/selenium/automation/base/TestBase.java
@@ -4,29 +4,60 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import com.java.selenium.automation.utilities.LoadProps;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
-	public static WebDriver driver;
+	public static WebDriver driver = null;;
 	
 	public static void main(String[] args) throws Throwable{
-		WebDriverManager.edgedriver().setup();
-		driver = new EdgeDriver();
 		
-		driver.manage().window().maximize();
 		
-		driver.get(LoadProps.getData("url"));
+		setup("ite");
 		
-		WebElement searchBar = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
-		searchBar.sendKeys("Zelalem");
-		searchBar.sendKeys(Keys.ENTER);
+	//	WebElement searchBar = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
+		//searchBar.sendKeys("Zelalem");
+		//searchBar.sendKeys(Keys.ENTER);
 		
+		
+		 WebElement we = driver.findElement(By.linkText("Gmail"));
+		 we.click();
+		 
+		 WebElement we1 = driver.findElement(By.linkText("Sign in"));
+		 we1.click();
+		 
+		 WebElement we2 = driver.findElement(By.partialLinkText("Learn more about using Guest mode"));
+		 we2.click();
+		
+		//label[text()='Name']/parent::div/following-sibling::div[1]/input
+		//label[text()='Name']/parent::div/following-sibling::div[3]/input
 		
 		//driver.close();
+	}
+	
+	public static void setup(String browserType) throws Throwable{
+		
+		if(browserType.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}else if(browserType.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}else if(browserType.equals("ie")) {
+			WebDriverManager.iedriver().setup();
+			driver = new InternetExplorerDriver();
+		}else {
+			throw new Exception("Browser type is not vlaid, please pass chrome, edge or ie!");
+		}
+		
+		driver.manage().window().maximize();
+		driver.get(LoadProps.getData("url"));
+		
 	}
 
 }
